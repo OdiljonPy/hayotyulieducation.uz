be6a6bdf52adc11916ad36a6e34152a9719d4db8
Odiljon Abduvaitov <73192064+OdiljonPy@users.noreply.github.com>
2023-12-21 11:07:25 +0500

    Update settings.py
    db changed to sqlite3

--- app/settings.py
+++ app/settings.py
@@ -77,24 +77,24 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = { 
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'tasks_manager',
+#        'USER': 'postgres',
+#        'PASSWORD': 'JobSchedulerV3$',
+#        'HOST': 'localhost',
+#        'PORT': '5432'
+#    }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tasks_manager',
-        'USER': 'postgres',
-        'PASSWORD': 'JobSchedulerV3$',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': 'mydatabase',
+     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'mydatabase',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
